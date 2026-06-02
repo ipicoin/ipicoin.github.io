6bab6e166298fbaffaf662e2459616a3eb11cffe
Sett Sarverott <sett@sarverott.com>
2026-06-02 03:28:42 +0200

    update project setups, remove stuff from template, preparements for further workaround

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({})
 const app = createApp(App)
 
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
