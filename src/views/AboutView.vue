b8fda2a9bd110d6a7ee600bdf179447c9824d5a5
Sett Sarverott <sett@sarverott.com>
2026-06-02 04:41:16 +0200

    update of workflows, making blank views for now

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <section>
+    <article>x</article>
+  </section>
+  <section>
+    <article>x</article>
+  </section>
+  <section>
+    <article>x</article>
+  </section>
+</template>
+
+<style module></style>
