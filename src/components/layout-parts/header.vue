c1a74e941a42212a523647fa5b5110a6cc033423
Sett Sarverott <sett@sarverott.com>
2026-06-02 04:18:42 +0200

    reshaping structures to be more similar targeted, autmated fixer had to be turned off, changing some internal parts

---
+++ src/components/layout-parts/header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div></div>
+    <div>
+      <nav></nav>
+    </div>
+  </header>
+</template>
+
+<style module></style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import './assets/main.css'
 
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
+// import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
-const i18n = createI18n({})
+// const i18n = createI18n({})
 const app = createApp(App)
 
-app.use(i18n)
-app.use(createPinia())
+// app.use(i18n)
+// app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
